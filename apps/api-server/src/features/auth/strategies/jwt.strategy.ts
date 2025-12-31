05533ea44a49162949283bad759fa96f1468e494
desmond <desm.swe@gmail.com>
2025-12-31 15:14:23 +0800

    feat(api-server): add ban check for apis

--- apps/api-server/src/features/auth/strategies/jwt.strategy.ts
+++ apps/api-server/src/features/auth/strategies/jwt.strategy.ts
@@ -7,6 +7,7 @@ import { Strategy } from 'passport-jwt';
 
 import { AuthConfig, type TAuthConfig } from '@/configs/auth.config';
 import { CustomHttpException } from '@/custom.exception';
+import { BanActionRepository } from '@/persistence/repos/ban';
 import { UserRepository } from '@/persistence/repos/user';
 import { IClsStore } from '@/types/cls';
 
@@ -19,6 +20,7 @@ export class JwtStrategy extends PassportStrategy(Strategy, ACCESS_TOKEN_STRATEG
     @AuthConfig() authConfig: TAuthConfig,
     private readonly clsService: ClsService<IClsStore>,
 
+    private readonly banActionRepository: BanActionRepository,
     private readonly userRepository: UserRepository
   ) {
     super({
@@ -47,7 +49,15 @@ export class JwtStrategy extends PassportStrategy(Strategy, ACCESS_TOKEN_STRATEG
       );
     }
 
-    // todo: add ban check
+    const now = new Date();
+    const isBanned = await this.banActionRepository.validateIfUserIsBanned(userId, now);
+
+    if (isBanned) {
+      throw new CustomHttpException(
+        `You have been banned or suspended`,
+        HttpErrorCode.FREEZE_ACCOUNT
+      );
+    }
 
     this.clsService.set('user.email', user.userEmail);
     this.clsService.set('user.id', user.userId);
