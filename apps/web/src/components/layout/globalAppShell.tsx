42905a2ee5733f5206be7db62961a31fc68ed01c
Yung Yau <yylee2998@gmail.com>
2026-01-07 00:23:49 +0800

    feat(web): add role application

--- apps/web/src/components/layout/globalAppShell.tsx
+++ apps/web/src/components/layout/globalAppShell.tsx
@@ -1,15 +1,112 @@
-import { Anchor, AppShell, Burger, Group, Image, NavLink, ScrollArea, Title } from '@mantine/core';
+import {
+  Anchor,
+  AppShell,
+  Button,
+  Burger,
+  FileInput,
+  Group,
+  Image,
+  Modal,
+  NavLink,
+  ScrollArea,
+  Select,
+  Stack,
+  Textarea,
+  Title,
+  Flex,
+} from '@mantine/core';
+import { useForm } from '@mantine/form';
 import { useDisclosure } from '@mantine/hooks';
+import { notifications } from '@mantine/notifications';
+import {
+  APPLY_ROLE_URL,
+  applyRoleRoSchema,
+  GET_ROLES_URL,
+  TApplyRoleRo,
+  TGetRolesVo,
+} from '@peernest/contract';
+import { UserRole } from '@peernest/core';
+import { IconArrowRight } from '@tabler/icons-react';
+import { useMutation, useQuery } from '@tanstack/react-query';
+import { zod4Resolver } from 'mantine-form-zod-resolver';
 import { Link } from 'react-router';
 
 import logoImage from '@/assets/logo.svg';
+import api from '@/lib/api-client';
 
 type TGlobalAppShellProps = {
   children: React.ReactNode;
 };
 
+// 1. call api
+async function getRole() {
+  return api.get<TGetRolesVo>(GET_ROLES_URL);
+}
+
+async function applyRole(data: TApplyRoleRo) {
+  const res = await api.post<void>(APPLY_ROLE_URL, data);
+  return res.data;
+}
+
 export default function GlobalAppShell({ children }: TGlobalAppShellProps) {
   const [opened, { toggle }] = useDisclosure();
+  const [modalOpened, modalHandlers] = useDisclosure(false);
+
+  // 2. data fetching library
+  const { data: roleData } = useQuery({
+    queryKey: ['roles'],
+    queryFn: async () => {
+      const res = await getRole();
+      return res.data;
+    },
+  });
+
+  // 3. form handling
+  // set values for all fields
+  // validate all fields
+  const applyRoleForm = useForm<TApplyRoleRo>({
+    initialValues: {
+      roleId: '',
+      description: '',
+      attachmentId: null,
+    },
+    validate: zod4Resolver(applyRoleRoSchema),
+  });
+
+  const applyRoleMutation = useMutation<void, Error, TApplyRoleRo>({
+    mutationFn: (data) => applyRole(data),
+    onSuccess: (_, variables) => {
+      console.log(variables);
+      const roleName = roleData?.find((role) => role.roleId === variables.roleId)?.roleName;
+
+      notifications.show({
+        message: `Your application for ${roleName ?? 'Unknown'} role is submitted!`,
+      });
+
+      modalHandlers.close();
+      applyRoleForm.reset();
+    },
+    onError: () => {
+      console.log('error');
+      notifications.show({
+        message: `Error: Your application is failed to submit. Please try again.`,
+      });
+
+      modalHandlers.close();
+      applyRoleForm.reset();
+    },
+  });
+
+  async function onSubmit(data: TApplyRoleRo) {
+    applyRoleMutation.mutate(data);
+  }
+
+  const roleNames = roleData
+    ?.filter((role) => role.roleName !== UserRole.User)
+    .map((role) => ({
+      label: role.roleName.charAt(0).toUpperCase() + role.roleName.slice(1),
+      value: role.roleId,
+    }));
 
   return (
     <AppShell
@@ -49,7 +146,59 @@ export default function GlobalAppShell({ children }: TGlobalAppShellProps) {
               />
             ))}
         </AppShell.Section>
-        <AppShell.Section p='md'>Navbar footer - always at the bottom</AppShell.Section>
+        <AppShell.Section p='md'>
+          <Modal
+            opened={modalOpened}
+            onClose={modalHandlers.close}
+            title='Apply a Role'
+            centered
+            padding={'md'}
+            size={720}>
+            <form onSubmit={applyRoleForm.onSubmit(onSubmit)}>
+              <Stack gap='xl'>
+                <Select
+                  label='Role'
+                  withAsterisk
+                  placeholder='Choose the role you would like to apply'
+                  data={roleNames}
+                  {...applyRoleForm.getInputProps('roleId')}
+                />
+                <Textarea
+                  label='Description'
+                  withAsterisk
+                  description='Introduce yourself and explain why you are qualified for the chosen role.'
+                  placeholder='What make you as a good candidate for the role...'
+                  minRows={4}
+                  maxRows={4}
+                  autosize
+                  {...applyRoleForm.getInputProps('description')}
+                />
+                <FileInput
+                  label='File'
+                  description='Only 1 file (< 10MB) is allowed.'
+                  placeholder='Pick File'
+                  accept='application/pdf,image/*'
+                  w={'fit-content'}
+                />
+                <Flex direction={'row-reverse'}>
+                  <Button w={'fit-content'} variant='filled' type='submit'>
+                    Submit
+                  </Button>
+                </Flex>
+              </Stack>
+            </form>
+          </Modal>
+          <Button
+            variant='filled'
+            size='md'
+            radius='md'
+            fw='400'
+            w={'100%'}
+            rightSection={<IconArrowRight size={16} />}
+            onClick={modalHandlers.open}>
+            Apply Role
+          </Button>
+        </AppShell.Section>
       </AppShell.Navbar>
       <AppShell.Main>{children}</AppShell.Main>
     </AppShell>
