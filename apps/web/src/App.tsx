a33fe838f563b50f1780fcc4e2ecb8de0bf3d152
Chew Wei Zhi <robinchew2203@gmail.com>
2026-01-22 12:52:59 +0800

    feat(web): peer matching searching user UI

--- apps/web/src/App.tsx
+++ apps/web/src/App.tsx
@@ -5,22 +5,14 @@ import LoginPage from '@/components/pages/auth/signInPage';
 import BanPage from '@/components/pages/ban/banPage';
 import Error404 from '@/components/pages/error/error404';
 import HomePage from '@/components/pages/homePage';
+import ResourcePage from '@/components/pages/playground/resources';
+import MyProfileEditingPage from '@/components/pages/user/myProfileEditing';
 import MyProfilePage from '@/components/pages/user/myProfilePage';
 import PeerMatchingPage from '@/components/pages/user/peerMatchingPage';
 import PeerProfilePreviewPage from '@/components/pages/user/peerProfilePreviewPage';
 import { APP_ROUTE } from '@/lib/app-route';
 
 export function App() {
-  // const timezone = Intl.DateTimeFormat().resolvedOptions().timeZone;
-  // console.log(timezone);
-
-  // const now = new Date();
-  // const local = new Date(now).toLocaleString('en-US', {
-  //   timeZone: timezone,
-  // });
-
-  // console.log({ now: now.toUTCString(), local });
-
   return (
     <Routes>
       <Route index element={<Navigate to={APP_ROUTE.HOME} />} />
@@ -29,8 +21,16 @@ export function App() {
       <Route element={<Layout />}>
         <Route path={APP_ROUTE.HOME} element={<HomePage />} />
         <Route path={APP_ROUTE.USER} element={<PeerMatchingPage />} />
+
+        {/* My profile */}
         <Route path={APP_ROUTE.USER_ME} element={<MyProfilePage />} />
+        <Route path={`${APP_ROUTE.USER_ME}/edit`} element={<MyProfileEditingPage />} />
+
+        {/* Peer profile preview */}
         <Route path={`${APP_ROUTE.USER}/:userId`} element={<PeerProfilePreviewPage />} />
+
+        {/* temporary page for testing */}
+        <Route path='resources' element={<ResourcePage />} />
       </Route>
 
       <Route path={APP_ROUTE.BAN} element={<BanPage />} />
