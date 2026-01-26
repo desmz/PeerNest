b89c136c9c604d43b050496f31442b1ba32ef9b4
desmond <desm.swe@gmail.com>
2026-01-26 19:11:14 +0800

    feat(web): add mood & wellness check in form UI

---
+++ apps/web/src/components/pages/wellness/moodAndWellnessPage.tsx
@@ -0,0 +1,3 @@
+export default function MoodAndWellnessPage() {
+  return 'this is mood page';
+}
--- apps/web/src/lib/util.ts
+++ apps/web/src/lib/util.ts
@@ -2,6 +2,11 @@ export function capitalizeFirstLetter(str: string) {
   if (!str) return ''; // Handle empty or null strings
   return str
     .split(' ')
-    .map((s) => s.charAt(0).toUpperCase() + s.slice(1))
+    .map((s) => capitalizeFirstLetterForFirstWord(s))
     .join(' ');
+}
+
+export function capitalizeFirstLetterForFirstWord(str: string) {
+  if (!str) return '';
+  return str.charAt(0).toUpperCase() + str.slice(1);
 }
